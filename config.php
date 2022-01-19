9ee98daac539a5beb9e556d144f01010444085a7
hamidreza_mj <hamid.mj313@gmail.com>
2022-01-19 17:34:33 +0330

    first commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+class Config
+{
+    public const USERNAME = ''; // must be changed to GitHub user name  ***[required]***
+    public const password = ''; // if needed place your GitHub password [optional]
+}
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once __DIR__ . '/config.php';
+
+$usernameAuth = Config::USERNAME;
+$passAuth = Config::password;
+
+$baseUrl = "https://api.github.com/users/$usernameAuth/";
+$followingUrl = "${baseUrl}following";
+$followersUrl = "${baseUrl}followers";
+
+getFollowers($followersUrl, $usernameAuth, $passAuth);
+
+function getFollowers($url, $username, $pass)
+{
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, $username . ':' . $pass);
+
+    $output = curl_exec($curl);
+
+    $info = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    $result = json_decode($output);
+
+    curl_close($curl);
+
+    echo '<pre>' . print_r($result, true) . '</pre>';
+}
+
+function getFollowing($url, $username, $pass)
+{
+    $curl = curl_init($url);
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, $username . ':' . $pass);
+
+    $output = curl_exec($curl);
+
+    $info = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $result = json_decode($output);
+
+    curl_close($curl);
+
+    echo '<pre>' . print_r($result, true) . '</pre>';
+}
+
+function getCurlError($curl)
+{
+    $errorNo = curl_errno($curl);
+    echo curl_strerror($errorNo);
+}
